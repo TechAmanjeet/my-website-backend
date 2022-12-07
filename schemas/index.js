3af4a7f57c951cd8f18fe472047079dff876a81d
Amanjeet kumar <techamanjeet1@gmail.com>
2022-12-08 02:09:33 +0530

    added all the schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import schemas from "./schemas";
+
+export const schemaTypes = schemas
---
+++ schemas/schemas.js
@@ -0,0 +1,13 @@
+import testimonials from './testimonials'
+import brands from './brands'
+import contacts from './contacts'
+import works from './works'
+import workExperiences from './workExperiences'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import todos from './todos'
+
+const schemas = [testimonials, contacts, brands, works, workExperiences, abouts, experiences, skills, todos]
+
+export default schemas
